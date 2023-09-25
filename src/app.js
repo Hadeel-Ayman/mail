69e6f8ea9e23e2e5999473b488671767a996b57f
Hadeel-Ayman <haddelayman4@gmail.com>
2023-09-26 02:09:11 +0300

    add files

--- src/app.js
+++ src/app.js
@@ -7,12 +7,12 @@ const port = process.env.PORT || 5000;
 require("../db/db");
 
 app.use("/api/email", emailRouter);
-app.use(cors(
-    (req, res, next) => {
-        res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-        next();
-    }));
+app.use(cors())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 // Start your server
 app.listen(port, () => {
