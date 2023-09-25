0eff109eb18f7f4fdfb7c4cc21596b35c8170816
Hadeel-Ayman <haddelayman4@gmail.com>
2023-09-26 01:19:04 +0300

    add files

--- src/app.js
+++ src/app.js
@@ -3,15 +3,15 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 const emailRouter = require("../Route/emailRoute");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 require("../db/db");
 
 app.use("/api/email", emailRouter);
 app.use(
     cors({
         origin: 'http://localhost:3000',
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        credentials: true, // Enable cookies and credentials for the request (if needed)
+        methods: 'GET,PUT,PATCH,POST,DELETE',
+        // credentials: true, // Enable cookies and credentials for the request (if needed)
     })
 );
 
