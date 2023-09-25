3016de3ada9c46667086f6255ec4519e6bfcb5c5
Hadeel-Ayman <haddelayman4@gmail.com>
2023-09-26 01:01:06 +0300

    add files

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 5000;
@@ -6,7 +7,11 @@ const emailRouter = require("../Route/emailRoute");
 require("../db/db");
 
 app.use("/api/email", emailRouter);
-
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+    })
+);
 
 app.listen(port, () => {
     console.log("This Application is running On Localhost  " + port);
