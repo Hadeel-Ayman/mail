42bdc2fa2ee2c4b76eb879f6802cb7245afdd91b
Hadeel-Ayman <haddelayman4@gmail.com>
2023-09-26 01:11:39 +0300

    add files

--- src/app.js
+++ src/app.js
@@ -2,16 +2,32 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(express.json());
-const port = process.env.PORT || 5000;
 const emailRouter = require("../Route/emailRoute");
+const port = process.env.PORT || 3000;
 require("../db/db");
 
 app.use("/api/email", emailRouter);
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: 'http://localhost:3000',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true, // Enable cookies and credentials for the request (if needed)
     })
 );
+
+// ... Your other routes and middleware ...
+
+// Start your server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
+
+
+
+
+
 
 app.listen(port, () => {
     console.log("This Application is running On Localhost  " + port);
